52a804d287688f175b3c545d0e0cb5d664effd4a
Jose <jose_smt_92@hotmail.com>
2022-05-19 01:10:00 +0200

    CQRS has been added.

---
+++ src/modules/shared/infraestructure/cqrs/InMemory/QueryHandlersInformation.ts
@@ -0,0 +1,21 @@
+import Query from '../../../domain/cqrs/Query'
+import QueryHandler from '../../../domain/cqrs/QueryHandler'
+import Response from '../../../domain/cqrs/Response'
+
+export default class QueryHandlersInformation {
+    private handlersMap: Map<Query, QueryHandler<Query, Response>>
+
+    constructor(handlers: Array<QueryHandler<Query, Response>>) {
+        this.handlersMap = new Map()
+        handlers.forEach((h) => this.handlersMap.set(h.subscribedTo(), h))
+    }
+
+    search(query: Query): QueryHandler<Query, Response> {
+        const handler = this.handlersMap.get(query)
+        if (!handler) {
+            throw new Error(`No Query Handler found for query <${query.constructor.name}>`)
+        }
+
+        return handler
+    }
+}
