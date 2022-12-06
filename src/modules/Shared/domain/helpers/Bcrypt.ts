083197393b95b013c855b8f7cc5b75d10eb8148c
Jose <jose_smt_92@hotmail.com>
2022-12-06 11:46:24 +0100

    Fix to improve code readability.

--- src/modules/Shared/domain/helpers/Bcrypt.ts
+++ src/modules/Shared/domain/helpers/Bcrypt.ts
@@ -1,9 +1,11 @@
 import bcrypt from 'bcrypt'
 
 export default class Bcrypt {
+    private static ROUNDS = 10
+
     generateHash(password: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            bcrypt.hash(password, 10, (err, hash) => {
+            bcrypt.hash(password, Bcrypt.ROUNDS, (err, hash) => {
                 if (err) {
                     reject(err)
                 }
